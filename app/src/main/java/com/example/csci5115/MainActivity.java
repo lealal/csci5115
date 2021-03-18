0bfddd56e63086b8ee63d344b3f621f9b35235ec
Adrian Leal <adrianlealalonse@gmail.com>
2021-03-18 13:54:47 -0500

    view item

--- app/src/main/java/com/example/csci5115/MainActivity.java
+++ app/src/main/java/com/example/csci5115/MainActivity.java
@@ -29,4 +29,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, EditItems.class);
         startActivity(intent);
     }
+
+    public void viewItem(View view){
+        Intent intent = new Intent(this, ViewItem.class);
+        startActivity(intent);
+    }
 }
