af544bff40c361931fc2d6d3e93d8b6497978878
Adrian Leal <adrianlealalonse@gmail.com>
2021-03-17 23:58:05 -0500

    Created Add Items activity window

---
+++ app/src/main/java/com/example/csci5115/AddItems.java
@@ -0,0 +1,14 @@
+package com.example.csci5115;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddItems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_items);
+    }
+}
--- app/src/main/java/com/example/csci5115/MainActivity.java
+++ app/src/main/java/com/example/csci5115/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.csci5115;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Call when the user taps the Add Item button
+    public void addItem(View view){
+        Intent intent = new Intent(this, AddItems.class);
+        startActivity(intent);
     }
 }
