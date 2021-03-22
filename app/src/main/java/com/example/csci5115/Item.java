fbd3d00c11d8c79d29db59a67976d9da419dbd90
Azazel5 <subhanga2013@gmail.com>
2021-03-22 17:58:11 -0500

    Added ItemAdapter and Item java code

--- app/src/main/java/com/example/csci5115/Item.java
+++ app/src/main/java/com/example/csci5115/Item.java
@@ -1,4 +1,43 @@
 package com.example.csci5115;
 
+import java.util.Date;
+
 public class Item {
+    private String itemName;
+    private Date itemAddedDate;
+    private String location;
+
+    public Item() {
+
+    }
+
+    public Item(String itemName, Date itemAddedDate, String location) {
+        this.itemName = itemName;
+        this.itemAddedDate = itemAddedDate;
+        this.location = location;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Date getItemAddedDate() {
+        return itemAddedDate;
+    }
+
+    public void setItemAddedDate(Date itemAddedDate) {
+        this.itemAddedDate = itemAddedDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- app/src/main/java/com/example/csci5115/ItemAdapter.java
+++ app/src/main/java/com/example/csci5115/ItemAdapter.java
@@ -1,4 +1,49 @@
 package com.example.csci5115;
 
-public class ItemAdapter {
+import java.util.List;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.MyViewHolder> {
+
+        private List<Item> itemList;
+
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+            public TextView title, year, genre;
+
+            public MyViewHolder(View view) {
+                super(view);
+                title = (TextView) view.findViewById(R.id.title);
+                genre = (TextView) view.findViewById(R.id.genre);
+                year = (TextView) view.findViewById(R.id.year);
+            }
+        }
+
+        public ItemAdapter(List<Item> itemList) {
+            this.itemList = itemList;
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.movie_list_row, parent, false);
+
+            return new MyViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            Item item = itemList.get(position);
+            holder.title.setText(movie.getTitle());
+            holder.genre.setText(movie.getGenre());
+            holder.year.setText(movie.getYear());
+        }
+
+        @Override
+        public int getItemCount() {
+            return itemList.size();
+        }
+    }
 }
