7c5cde5a1491fabf240913c37d1f4fee75d2e0c8
Adrian Leal <adrianlealalonse@gmail.com>
2021-03-28 17:23:22 -0500

    resolve conflict

--- app/src/main/java/com/example/csci5115/AddItems.java
+++ app/src/main/java/com/example/csci5115/AddItems.java
@@ -2,7 +2,9 @@ package com.example.csci5115;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddItems extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class AddItems extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_items);
+    }
+
+    public void scanReceipt(View view){
+        Intent intent = new Intent(this, ScanReceipt.class);
+        startActivity(intent);
+    }
+
+    public void itemCategories(View view){
+        Intent intent = new Intent(this, ItemCategories.class);
+        startActivity(intent);
     }
 }
