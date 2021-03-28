0647ddc054fd15815da963ef166b886a1299a1d4
Jeffrey Jia <ijeffreyjia@gmail.com>
2021-03-27 21:02:14 -0500

    new pages

---
+++ app/src/main/java/com/example/csci5115/ItemCategories.java
@@ -0,0 +1,14 @@
+package com.example.csci5115;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemCategories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_categories);
+    }
+}
---
+++ app/src/main/java/com/example/csci5115/ScanReceipt.java
@@ -0,0 +1,14 @@
+package com.example.csci5115;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScanReceipt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_receipt);
+    }
+}
