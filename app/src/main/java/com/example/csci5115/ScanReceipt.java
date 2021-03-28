39184ee2ebfbb93d68eec8fcce332219a1382394
Adrian Leal <adrianlealalonse@gmail.com>
2021-03-28 17:23:50 -0500

    resolve conflict

--- app/src/main/java/com/example/csci5115/ScanReceipt.java
+++ app/src/main/java/com/example/csci5115/ScanReceipt.java
@@ -2,7 +2,9 @@ package com.example.csci5115;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ScanReceipt extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class ScanReceipt extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_receipt);
+    }
+
+    public void addScanItems(View view){
+        AddItems.addedItems.add("Dragon Fruit");
+        AddItems.addedItems.add("Bananas");
+        AddItems.addedItems.add("Chicken");
+        Intent intent = new Intent(this, AddItems.class);
+        startActivity(intent);
     }
 }
